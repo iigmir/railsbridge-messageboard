85fd7762495b6516ecd14492462ce77ca9c98c05
iigmir <roc120j@gmail.com>
2017-03-17 15:21:43 +0800

    Install Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-    # resource "home"
-    root "home#index"
+  devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resource "books"
+  # resource "readers"
+  root "home#index"
 end
