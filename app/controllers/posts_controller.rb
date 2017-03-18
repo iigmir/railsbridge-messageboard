780482c4ff591157f0b9a4ebd0308379698dbb1e
iigmir <roc120j@gmail.com>
2017-03-18 17:34:34 +0800

    just can post thread -- but how can i get correct link?

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,28 @@
 class PostsController < ApplicationController
+    # C for CRUD
+    def new
+        @posts = Post.new
+    end
+    def create
+        @posts = Post.create(post_params)
+        if @posts.save
+           redirect_to posts_url
+        else
+           render 'new'
+        end
+    end
+
+    # R for CRUD
+    def index
+        @posts = Post.all
+    end
+    def show
+        @posts = Post.all
+        #@posts = Post.find_by_id(params[:id])
+    end
+
+    private
+    def post_params
+        params.require(:post).permit(:title, :content, :poster)
+    end
 end
